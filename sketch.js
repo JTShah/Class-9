402ecf08d210aa56a3b4ea4c42d26b3026f2133d
JTShah <133907293+JTShah@users.noreply.github.com>
2023-05-17 18:58:48 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function preload() {
+//used to load different images, videos and sounds (all media)
+}
+
+function setup () {
+  //used to create various sprites (and the canvas itself!!)
+
+  createCanvas(1000,700);
+  
+}
+
+function draw() {
+  //used to put main content in the game. 
+  background("#810fab");
+}
